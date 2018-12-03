a5a775882246d7c1f0c879a30f6faedf52effadf
ChoiSoYoung <37925497+Menboong@users.noreply.github.com>
2018-12-03 18:36:58 +0900

    Delete pwmfunc.h

--- Choi_So_Young/pwmfunc.h
+++
@@ -1,9 +0,0 @@
-#define GPIOC Motorport
-#define GPIO_PIN_0 MotorA1
-#define GPIO_PIN_1 MotorA2
-#define GPIO_PIN_2 MotorB1
-#define GPIO_PIN_3 MotorB2
-
-void Dir_control(uint8_t str[]);
-uint16_t Conversion(uint8_t str[]);
-void speed(uint16_t val);
